2f8971029449aaa165778d6521884a54bb6c1eb8
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2017-10-08 07:25:41 +0200

    cleanup code

--- run.sh
+++ run.sh
@@ -5,23 +5,28 @@ curl -s https://raw.githubusercontent.com/sindresorhus/binary-extensions/master/
 extensions=($(find . -type f | egrep -i -E -o '\w*$' | sort -su))
 
 # check tracked files
-if [ -f repo/.gitattributes ]
-then
+if [[ -f repo/.gitattributes ]]; then
   cut -d ' ' -f1 repo/.gitattributes | sed 's/^\*\.//g' > tracked
 else
   touch tracked
 fi
 
-FILE=binaries
-TRACKED=tracked
-
+file=binaries
+lfs=tracked
 # loop all extensions
-for i in ${extensions[@]}
-do
-  if grep -Fxq $i "$TRACKED"; then
+for i in ${extensions[@]}; do
+  if grep -Fxq $i "$lfs"; then
     continue
   fi
-  if grep -Fxq $i "$FILE"; then
-    >&2 echo $i
+  if grep -Fxq $i "$file"; then
+    error=1
+    echo $i 1>&2
   fi
 done
+
+# set exit code
+if [[ $error ]]; then
+  exit 1
+else
+  exit 0
+fi
